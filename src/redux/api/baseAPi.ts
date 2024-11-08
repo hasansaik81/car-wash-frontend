4490e584a840f23c044ace965794a962c1a30515
hasansaik81 <hasansaik81@gmail.com>
2024-11-07 22:07:12 -0800

    first commit

---
+++ src/redux/api/baseAPi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: " https://y-hazel-three.vercel.app",
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  tagTypes: ["Rating", "Service", "Slot", "User", "Booking"],
+  endpoints: () => ({}),
+});
+
+export default baseApi;
+
+// https://y-cvefwhbru-hasans-projects-3bda48be.vercel.app/
